b8f99ce610d1a24718d748534e0da85121c02d14
NiroAmir <nir_amir9@hotmail.com>
2022-08-03 20:47:37 +0300

    add let

---
+++ app.js
@@ -0,0 +1,16 @@
+let currentPageId = 'home-page'
+
+function makeMenuLinkChangePage(linkId, pageToHide, pageToShow) {
+  console.log(linkId, pageToHide, pageToShow);
+  const pageLinkElement = document.getElementById(linkId);
+
+  pageLinkElement.addEventListener('click', function () {
+    console.log('you clicked me');
+    document.getElementById(pageToShow).classList.remove('d-none');
+
+    document.getElementById(pageToHide).classList.add('d-none');
+  });
+}
+
+makeMenuLinkChangePage('home-page-link', 'about-page', 'home-page');
+makeMenuLinkChangePage('about-page-link', 'home-page', 'about-page');
