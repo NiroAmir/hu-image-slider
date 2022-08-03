bb694cd83146c1b3c916bb5cb3acc1c2481d555f
NiroAmir <nir_amir9@hotmail.com>
2022-08-03 21:08:40 +0300

    new page

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-let currentPageId = 'home-page'
+let currentPageId = 'home-page';
 
-function makeMenuLinkChangePage(linkId, pageToHide, pageToShow) {
-  console.log(linkId, pageToHide, pageToShow);
+function makeMenuLinkChangePage(linkId, pageToShow) {
   const pageLinkElement = document.getElementById(linkId);
 
   pageLinkElement.addEventListener('click', function () {
     console.log('you clicked me');
     document.getElementById(pageToShow).classList.remove('d-none');
 
-    document.getElementById(pageToHide).classList.add('d-none');
+    document.getElementById(currentPageId).classList.add('d-none');
+
+    currentPageId = pageToShow;
   });
 }
 
-makeMenuLinkChangePage('home-page-link', 'about-page', 'home-page');
-makeMenuLinkChangePage('about-page-link', 'home-page', 'about-page');
+makeMenuLinkChangePage('home-page-link', 'home-page');
+makeMenuLinkChangePage('about-page-link', 'about-page');
+makeMenuLinkChangePage('contact-page-link', 'contact-page');
