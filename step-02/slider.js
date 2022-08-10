497e209df8cfb0630d6546cc6ee18803af11c47a
NiroAmir <nir_amir9@hotmail.com>
2022-08-10 20:20:26 +0300

    add event listeners to the image slider btn

--- step-02/slider.js
+++ step-02/slider.js
@@ -1,1 +1,12 @@
 console.log('slider js');
+
+function prevImage(){
+    console.log('prev clicked');
+}
+function nextImage(){
+    console.log('next clicked');
+}
+
+
+document.getElementById('prevBtn').addEventListener('click', prevImage);
+document.getElementById('nextBtn').addEventListener('click', nextImage);
