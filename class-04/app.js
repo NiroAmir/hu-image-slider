3c5998117e414d7f3d4f6d4919f86a216e437ad5
NiroAmir <nir_amir9@hotmail.com>
2022-08-17 17:52:58 +0300

    changing folders name

---
+++ class-04/app.js
@@ -0,0 +1,29 @@
+let currentPageId = 'home-page';
+
+function showPage(pageIdToShow) {
+  console.log('you clicked me');
+  if (currentPageId == pageIdToShow) {
+    return;
+  }
+
+  console.log(' after the if');
+
+  // Show the page
+  document.getElementById(pageIdToShow).classList.remove('d-none');
+
+  // Hide the page
+  document.getElementById(currentPageId).classList.add('d-none');
+
+  currentPageId = pageIdToShow;
+}
+
+function makeMenuLinkChangePage(linkId, pageToShow) {
+  const pageLinkElement = document.getElementById(linkId);
+  pageLinkElement.addEventListener('click', function () {
+    showPage(pageToShow);
+  });
+}
+
+makeMenuLinkChangePage('home-page-link', 'home-page');
+makeMenuLinkChangePage('about-page-link', 'about-page');
+makeMenuLinkChangePage('contact-page-link', 'contact-page');
